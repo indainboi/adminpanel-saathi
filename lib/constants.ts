1a3bd13a19952afb97dd416d2bc9e6c4a0eeab00
jeet1703 <prabhjeetdec03@gmail.com>
2025-05-07 09:14:39 +0530

    Added one mail

--- lib/constants.ts
+++ lib/constants.ts
@@ -1,2 +1,2 @@
 // lib/constants.ts
-export const allowedEmails = ["prabhjeetdec03@gmail.com","adityasinghrajput7@gmail.com","shivam.munshi1@gmail.com","shreyas.garain@gmail.com","puneetkumarjh@gmail.com"];
+export const allowedEmails = ["prabhjeetdec03@gmail.com","adityasinghrajput7@gmail.com","shivam.munshi1@gmail.com","shreyas.garain@gmail.com","puneetkumarjh@gmail.com","ahmedrahul007@gmail.com "];
